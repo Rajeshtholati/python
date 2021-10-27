193fa9d49653cdecc4950c1610fe99aea3fad8c6
Rajeshtholati <92287970+Rajeshtholati@users.noreply.github.com>
2021-10-27 16:24:54 +0530

    Add files via upload

---
+++ nathural even odd give number in compiling chamber.py
@@ -0,0 +1,9 @@
+n=int(input('enter a number:'))
+p=n*(n+1)//2
+q=n*(n+1)
+r=n**2
+s=p+q+r
+print("sum of",n,"nathural numbers is",p)
+print("sum of",n,"even nathural numbers is",q)
+print("sum of",n,"odd nathural numbers is",r)
+print("total sum of",n,"nathural numbers ,even nathural numbers,odd nathural numbers is",s)
