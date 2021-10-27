860151d888c91a9d5376b118145fdb3e2046c340
Rajeshtholati <92287970+Rajeshtholati@users.noreply.github.com>
2021-10-27 10:34:02 +0530

    Add files via upload

---
+++ simple intrest.py
@@ -0,0 +1,6 @@
+# simple intrest program
+p=int(input("enter the principle"))
+t=int(input("enter the time(in years)"))
+r=float(input("enter the rate of intrest "))
+s=p*t*r/100
+print("simple intrest for",p,"amount",t,"period of time",r,"amount of intrest is",s)
