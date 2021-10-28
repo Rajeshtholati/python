204d0d655eb92cf0d9bb90c055986c224d555cbe
Rajeshtholati <92287970+Rajeshtholati@users.noreply.github.com>
2021-10-28 17:26:07 +0530

    Add files via upload

---
+++ to get english words of digits.py
@@ -0,0 +1,23 @@
+d=int(input('enter a number'))
+if d==0:
+    print('zero')
+elif d==1:
+    print('one')
+elif d==2:
+    print('two')
+elif d==3:
+    print('three')
+elif d==4:
+    print('four')
+elif d==5:
+    print('five')
+elif d==6:
+    print('six')
+elif d==7:
+    print('seven')
+elif d==8:
+    print('eight')
+elif d==9:
+    print('nine')
+else:
+    print('enter a digit not a number')
