d45a2979bfe3744b6ab07be031b1d151c1baeb96
Rajeshtholati <92287970+Rajeshtholati@users.noreply.github.com>
2021-10-28 16:41:59 +0530

    Add files via upload

---
+++ to find positive or negative.py
@@ -0,0 +1,35 @@
+number=int(input('enter a number:'))
+if number>=0:
+    print('it is a positive number')
+else:
+    print('it is a negative number')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
