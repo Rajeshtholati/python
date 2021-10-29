6e03a92c0ab328f45373b19f3ae33ead165961e7
Rajeshtholati <92287970+Rajeshtholati@users.noreply.github.com>
2021-10-29 16:37:32 +0530

    Add files via upload

---
+++ relational operators.py
@@ -0,0 +1,31 @@
+age=int(input('enter your age'))
+if(age<13):
+    print('child')
+elif(age>=13 and age<20):
+    print('teen')
+elif(age>=20 and age<50):
+    print('adult')
+else:
+    print('old')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
