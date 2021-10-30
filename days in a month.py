72f62ea940ccf5b9abfed88bd01d3c3f1bc5f28c
Rajeshtholati <92287970+Rajeshtholati@users.noreply.github.com>
2021-10-30 17:05:53 +0530

    Add files via upload

---
+++ days in a month.py
@@ -0,0 +1,25 @@
+m=int(input())
+if m==1:
+    print('month 1 has 31 days')
+elif m==2:
+    print('month 2 has 28 days')
+elif m==3:
+    print('month 3 has 31 days')
+elif m==4:
+    print('month 4 has 30 days')
+elif m==5:
+    print('month 5 has 31 days')
+elif m==6:
+    print('month 6 has 30 days')
+elif m==7:
+    print('month 7 has 31 days')
+elif m==8:
+    print('month 8 has 30 days')
+elif m==9:
+    print('month 9 has 31 days')
+elif m==10:
+    print('month 10 has 30 days')
+elif m==11:
+    print('month 11 has 31 days')
+elif m==12:
+    print('month 12 has 30 days')
