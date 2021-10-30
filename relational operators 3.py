0bffc456e388c1c9a52fc6e78e207d74899319af
Rajeshtholati <92287970+Rajeshtholati@users.noreply.github.com>
2021-10-30 17:21:57 +0530

    Add files via upload

---
+++ relational operators 3.py
@@ -0,0 +1,7 @@
+n=int(input())
+if n==4 or n==6 or n==9 or n==10 or n==11:
+    print('month',n,'has 30 days')
+elif n==2:
+    print('month',n,'has 28 days')
+else:
+    print('month',n,'has 31 days')
