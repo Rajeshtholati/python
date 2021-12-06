9863898cc1e84bff209d3860acfbf7b5da02c1be
Rajeshtholati <92287970+Rajeshtholati@users.noreply.github.com>
2021-12-06 17:10:16 +0530

    Update pattteerns to print 1 on both diagonals.py

--- pattteerns to print 1 on both diagonals.py
+++ pattteerns to print 1 on both diagonals.py
@@ -2,9 +2,7 @@
 n=int(input())
 for i in range(n):
     for j in range(n):
-       if i+j==n-1:
-           print(1,end=' ')
-       elif i==j:
+       if i+j==n-1 or i==j:
            print(1,end=' ')
        else:
            print(0,end=' ')
