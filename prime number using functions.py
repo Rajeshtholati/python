4ef969ce50fd4285c50b735089ab326eae88a98e
Rajeshtholati <92287970+Rajeshtholati@users.noreply.github.com>
2021-12-10 17:42:11 +0530

    Update prime number using functions.py

--- prime number using functions.py
+++ prime number using functions.py
@@ -3,10 +3,10 @@ def prime(n):
     for i in range(1,n+1):
         if n%i==0:
             fc+=1
-        if fc==2:
-            return True
-        else:
-            return False
+    if fc==2:
+        return True
+    else:
+        return False
 x=int(input())
 if prime(x)==True:
     print(x,'is prime')
