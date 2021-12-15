a4b7b19709ccec2a417d2cdd1f2a66220e21e446
Rajeshtholati <92287970+Rajeshtholati@users.noreply.github.com>
2021-12-15 17:11:42 +0530

    Add files via upload

---
+++ help.py
@@ -0,0 +1,5 @@
+def location(city,country='india',continent='asia'):
+    '''the program tells about location'''
+    print(city,'is located in',country,'which is in',continent)
+#function call
+location('shangai' )
